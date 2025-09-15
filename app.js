a13627fbbb5a502cb96038d8f47799aa14372bd6
nandinip92 <nandini.p8892@gmail.com>
2025-09-15 17:49:44 +0100

    chore: updating the file with schemas and resolvers

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const typeDefs = require("./schema"); // our GraphQL Schema
+const resolvers = require("./resolvers"); // our GraphQL resolvers
 
-const connectToDatabase = require('./database');
+const connectToDatabase = require("./database");
 
 async function startServer() {
-  //  // Connect to the database
-  await connectToDatabase();
-
   // Create an Express app
   const app = express();
 
   // Create an Apollo server
   const server = new ApolloServer({
-    typeDefs: /* GraphQL schema */,
-    resolvers: /* GraphQL resolvers */,
+    typeDefs,
+    resolvers,
   });
+
+  await server.start();
 
   // Apply the Apollo middleware to the Express app
   server.applyMiddleware({ app });
+
+  //  Connect to the database
+  await connectToDatabase();
 
   // Start the Express server
   app.listen({ port: 4000 }, () => {
